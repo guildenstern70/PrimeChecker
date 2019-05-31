f4cb44e668d998c686251068b052b7f378832543
Alessio Saltarin <alessiosaltarin@gmail.com>
2019-05-31 17:11:23 +0200

    Removed magic number

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 
 #include "PrimeChecker.h"
 
+#define MAX_NUMBER_TO_CHECK 100
+
 using namespace std;
 
 int main() {
     cout << "Prime Checker" << endl << endl;
 
-    for (int num=1; num<=100; num++)
+    for (int num=1; num<=MAX_NUMBER_TO_CHECK; num++)
     {
         if (PrimeChecker::check(num))
             cout << "> " << num << endl;
